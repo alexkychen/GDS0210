901671393dc113b89ae3fb5803854d3aebafed1d
alexkychen <alexkychen@gmail.com>
2017-02-20 15:09:14 -0500

    update

--- app.R
+++ app.R
@@ -1,14 +1,15 @@
 library(shiny)
+library(ggplot2)
 
 ui <- fluidPage(
+  titlePanel("Gene drive simulator"),
   #Input
   sidebarLayout(
     sidebarPanel(
-      sliderInput("popsize", label = "Population Size", value=10, min=1, max=100),
-      selectInput("popunit", label = "", choices = c("one"=1,"ten"=10,"hundred"=100,"thousand"=1000,"million"=1000000)),
-      #fileInput(inputId = "fileIn", label = "Upload your file"),
-      selectInput("repro.mode", label ="Reproduction mode", choices = c("Semelparity" = "semelparity","Iteroparity" = "iteroparity")),
-      actionButton("go", label="Make Plot")
+      numericInput(inputId = "popsize",label = "Wild population size (individual)", 100),
+      numericInput(inputId = "release",label = "Released gene drive (individual)", 20),
+      selectInput(inputId = "drawline", label = "Draw regression", choices = c("None"="none","Logistic regression"="logistic","Linear regression"="linear")),
+      actionButton("go", label="Simulate")
       
     ),
     mainPanel(
@@ -16,14 +17,60 @@ ui <- fluidPage(
       plotOutput(outputId = "plot1")
     )
   )
-  
 )
   
 server <- function(input, output){
   data <- eventReactive(input$go, {
-    size <- input$popsize * as.numeric(input$popunit)
-    title <- paste("Population size",input$popunit)
-    hist(rnorm(size), main=title)
+    wild <- round(input$popsize / input$release)
+    drive <- 1
+    #define a function to get the next gene drive generation
+    driveNextGen <- function(wild=NULL, drive=NULL){
+      F0 <- c(rep("W", wild*2), rep("G", drive*2)) #F0 gametes
+      F1 <- NULL
+      mixindex <- sample(length(F0))#create sampling index
+      for (i in seq(1, length(mixindex), 2)){
+        pair <- F0[mixindex[c(i, i+1)]] #Get every two individuals as a pair
+        if("W" %in% pair & "G" %in% pair){
+          pair <- c("G","G")
+          F1 <- c(F1, pair)
+        }else{
+          F1 <- c(F1, pair)
+        }
+      }
+      return(F1)
+    }
+    #Estimate the initial gene drive frequency and create a data frame to save results
+    driveFreq <- drive / (drive + wild)
+    df <- data.frame(0, driveFreq)
+    gen <- 0
+    
+    #Simulate gene drive freq across generations until drive freq = 1
+    while(!is.na(wild)){ #keep looping until wild becomes NA
+      gen <- gen + 1
+      res <- driveNextGen(wild=wild, drive=drive)
+      restb <- table(res)
+      wild <- restb[2]
+      drive <- restb[1]
+      driveFreq <- restb[1]/sum(restb) #restb[1] is restb[names(restb)=="G]
+      data <- c(gen, driveFreq)
+      df <- rbind(df, data)
+    }
+    #Create plot
+    ggplot(data=df, aes(x=X0, y=driveFreq))+
+      geom_point()+
+      ggtitle("Gene Drive Frequency Changes across Generations")+
+      xlab("Generation")+ylab("Gene drive frequency")+
+      ylim(0,1)+
+      scale_x_continuous(breaks=seq(0, nrow(df), 1))+
+      theme_bw()+
+      theme(axis.title = element_text(size=14),
+            axis.text = element_text(size=12))+
+      if(input$drawline == "logistic"){
+        stat_smooth(method="glm", method.args=list(family="binomial"), se=F)
+      } else if(input$drawline == "linear"){
+        stat_smooth(method = "lm", se = F)
+      }
+    
   })
   output$plot1 <- renderPlot({
     data()
